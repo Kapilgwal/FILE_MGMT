58183b8a2a62fd564439869824e0b793e7396fed
kapil <2909kapil2001@gmail.com>
2025-08-21 12:48:28 +0530

    Changes the os

--- management/management/settings.py
+++ management/management/settings.py
@@ -77,12 +77,9 @@ WSGI_APPLICATION = 'management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER' : 'postgres',
-        'PASSWORD' : '12345',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+
     }
 }
 
